02023f61f59953b8d182657441820e8e249c1724
Ganesh <ganesh@rockoder.com>
2014-11-12 22:36:26 +0530

    Added mirror, all path algorithms. Pretty display is still work in progress.

--- BinaryTree.h
+++ BinaryTree.h
@@ -27,10 +27,14 @@ public:
 	std::vector<T> preorder() const;
 
 	void displayLevelOrder() const;
+	void displayPretty() const;
+	void displayAllPaths() const;
 
 	size_t size() const;
 	size_t height() const;
 	size_t width() const;
+
+	void mirror();
 
 private:
 	void insert(const T& data);
@@ -40,10 +44,14 @@ private:
 	void preorder(std::shared_ptr<Node<T>> root, std::vector<T>& v) const;
 
 	void levelorder(std::queue<std::shared_ptr<Node<T>>>& q) const;
+	void displayPretty(std::queue<std::shared_ptr<Node<T>>>& q) const;
+	void displayAllPaths(std::shared_ptr<Node<T>> root, std::vector<T> v) const;
 
 	size_t size(std::shared_ptr<Node<T>> root) const;
 	size_t height(std::shared_ptr<Node<T>> root) const;
 	size_t width(std::shared_ptr<Node<T>> root) const;
+
+	void mirror(std::shared_ptr<Node<T>> root);
 
 protected:
 	std::shared_ptr<Node<T>> _root;
@@ -99,6 +107,24 @@ void BinaryTree<T>::displayLevelOrder() const
 }
 
 template<typename T>
+void BinaryTree<T>::displayPretty() const
+{
+	std::queue<std::shared_ptr<Node<T>>> q;
+
+	q.push(_root);
+	q.push(nullptr);
+
+	displayPretty(q);
+}
+
+template<typename T>
+void BinaryTree<T>::displayAllPaths() const
+{
+	std::vector<T> v;
+	displayAllPaths(_root, v);
+}
+
+template<typename T>
 size_t BinaryTree<T>::size() const
 {
 	return size(_root);
@@ -115,6 +141,14 @@ size_t BinaryTree<T>::width() const
 {
 	return width(_root);
 }
+
+template<typename T>
+void BinaryTree<T>::mirror()
+{
+	mirror(_root);
+}
+
+/////////// Private Member Functions ///////////
 
 template<typename T>
 void BinaryTree<T>::insert(const T& data)
@@ -228,6 +262,108 @@ void BinaryTree<T>::levelorder(std::queue<std::shared_ptr<Node<T>>>& q) const
 	}
 }
 
+// TODO: Works only for complete BT. Need to handle other cases.
+template<typename T>
+void BinaryTree<T>::displayPretty(std::queue<std::shared_ptr<Node<T>>>& q) const
+{
+	size_t h = 2 * height(q.front());
+	size_t level = 1;
+	bool firstNode = true;
+	char space = '-';
+
+	while (!q.empty())
+	{
+		std::shared_ptr<Node<T>> temp = q.front();
+		q.pop();
+
+		if (temp)
+		{
+			if (firstNode)
+			{
+				for (int i = 1; i < h / level; ++i)
+				{
+					std::cout << space;
+				}
+
+				firstNode = false;
+				std::cout << temp->data;
+			}
+			else
+			{
+				for (int i = 1; i < 2 * (h / level); ++i)
+				{
+					std::cout << space;
+				}
+
+				std::cout << temp->data;
+			}
+
+			if (temp->left)
+			{
+				q.push(temp->left);
+			}
+			else
+			{
+				//q.push(std::make_shared<Node<T>>(T{}));
+			}
+
+			if (temp->right)
+			{
+				q.push(temp->right);
+			}
+			else
+			{
+				//q.push(std::make_shared<Node<T>>(T{}));
+			}
+
+			if (!q.front())
+			{
+				q.pop();
+				std::cout << std::endl;
+				q.push(nullptr);
+
+				firstNode = true;
+				level *= 2;
+			}
+		}
+	}
+}
+
+template<typename T>
+void BinaryTree<T>::displayAllPaths(std::shared_ptr<Node<T>> root,
+					std::vector<T> v) const
+{
+	// 4 parts of recursion:
+
+	// 1. termination condition
+	if (!root)
+	{
+		return;
+	}
+
+	// 2. some task to perform
+	v.push_back(root->data);
+
+	// 3. generate result
+	// condition when to perform task can be different from
+	// termination condition.
+	if (!root->left && !root->right)
+	{
+		for (const auto& ele : v)
+		{
+			std::cout << ele << "\t";
+		}
+
+		std::cout << std::endl;
+		
+		return;
+	}
+
+	// 4. recursion call(s)
+	displayAllPaths(root->left, v);
+	displayAllPaths(root->right, v);
+}
+
 template<typename T>
 size_t BinaryTree<T>::size(std::shared_ptr<Node<T>> root) const
 {
@@ -268,6 +404,25 @@ size_t BinaryTree<T>::width(std::shared_ptr<Node<T>> root) const
 
 	return std::max(1 + leftHeight + rightHeight,
 		std::max(leftWidth, rightWidth));
+}
+
+template<typename T>
+void BinaryTree<T>::mirror(std::shared_ptr<Node<T>> root)
+{
+	// 3 parts of recursion:
+
+	// 1. termination condition
+	if (!root)
+		return;
+
+	// 2. some task to perform
+	std::shared_ptr<Node<T>> temp = root->right;
+	root->right = root->left;
+	root->left = temp;
+
+	// 3. recursion call
+	mirror(root->left);
+	mirror(root->right);
 }
 
 #endif
